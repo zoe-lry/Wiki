b68ac9cc58fd976001bc1d99ebd7ae83ff5a0c4f
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-03-20 16:21:59 -0700

    2-8 Adding customized config/增加自定义配置项示例

--- src/main/java/com/zoe/wiki/controller/TestController.java
+++ src/main/java/com/zoe/wiki/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.zoe.wiki.controller;
 
 
-import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //返回一个字符串
 public class TestController {
+
+  //增加一个自定义的配置项示例
+  //可以添加默认值， 如果没有配置会采用默认值
+  @Value("${test.hello: TEST}")
+  private String testHello;
+
 
   /**
    * HTTP请求总共有八种，常用的四种请求：GET, POST, PUT, DELETE
@@ -21,7 +27,7 @@ public class TestController {
 //   @RequestMapping(value = "/user/1", method = RequestMethod.GET)
   @RequestMapping("/hello")
   public String hello() {
-    return "Hello World";
+    return "Hello World " + testHello;
   }
   @PostMapping("/hello/post")
   public String helloPost(String name) {
