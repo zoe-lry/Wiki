2695415f05db7a09ce7838032805716bdfb5d5f4
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-03-20 15:57:03 -0700

    2-7 Adding Test interface test.http

--- src/main/java/com/zoe/wiki/controller/TestController.java
+++ src/main/java/com/zoe/wiki/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.zoe.wiki.controller;
 
 
+import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +12,19 @@ public class TestController {
   /**
    * HTTP请求总共有八种，常用的四种请求：GET, POST, PUT, DELETE
    *
-   * @RequestMapping 表示支持四种请求
-   * @GetMapping
-   * @DeleteMapping
-   * @PutMapping
    */
+//   @RequestMapping 表示支持四种请求
+//   @GetMapping
+//   @PostMapping
+//   @DeleteMapping
+//   @PutMapping
+//   @RequestMapping(value = "/user/1", method = RequestMethod.GET)
   @RequestMapping("/hello")
   public String hello() {
     return "Hello World";
+  }
+  @PostMapping("/hello/post")
+  public String helloPost(String name) {
+    return "Hello World! Post " + name;
   }
 }
