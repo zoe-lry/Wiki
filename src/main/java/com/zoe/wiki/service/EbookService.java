459dfad382a4907280743a7abd97b92cf306e5ad
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-03-22 16:53:58 -0700

    3-7 制作CopyUtil封装BeanUtils

--- src/main/java/com/zoe/wiki/service/EbookService.java
+++ src/main/java/com/zoe/wiki/service/EbookService.java
@@ -5,10 +5,9 @@ import com.zoe.wiki.domain.EbookExample;
 import com.zoe.wiki.mapper.EbookMapper;
 import com.zoe.wiki.req.EbookReq;
 import com.zoe.wiki.resp.EbookResp;
-import java.util.ArrayList;
+import com.zoe.wiki.util.CopyUtil;
 import java.util.List;
 import javax.annotation.Resource;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service  //识别是Service
@@ -21,12 +20,19 @@ public class EbookService {
     EbookExample.Criteria criteria = ebookExample.createCriteria();
     criteria.andNameLike("%" + req.getName() + "%");
     List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
-    List<EbookResp> respList = new ArrayList<>();
-    for (Ebook ebook : ebooksList) {
-      EbookResp ebookResp = new EbookResp();
-      BeanUtils.copyProperties(ebook, ebookResp);
-      respList.add(ebookResp);
-    }
+
+//    List<EbookResp> respList = new ArrayList<>();
+//    for (Ebook ebook : ebooksList) {
+
+////      EbookResp ebookResp = new EbookResp();
+////      BeanUtils.copyProperties(ebook, ebookResp);
+// //      //对象复制
+//      EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//      respList.add(ebookResp);
+//    }
+
+//    列表复制
+    List<EbookResp> respList = CopyUtil.copyList(ebooksList, EbookResp.class);
     return respList;
   }
 
