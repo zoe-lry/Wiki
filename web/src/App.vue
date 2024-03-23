973e331858ac6a666f03af46c1e0e86388984abf
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-03-23 09:50:59 -0700

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,16 +2,14 @@
   <a-layout id="components-layout-demo-top-side-2">
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center;height: 100px;width: 100%;position: absolute;bottom: 0;left: 0">
-      Zoe's Blog
-    </a-layout-footer>
+    <TheFooter></TheFooter>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
-
+import TheFooter from "@/components/the-footer.vue";
 
 export default defineComponent({
   name: 'app',
@@ -20,6 +18,7 @@ export default defineComponent({
   },
   components: {
     TheHeader,
+    TheFooter,
   },
 });
 </script>
@@ -34,5 +33,3 @@ export default defineComponent({
   float: left;
 }
 </style>
-<script setup lang="ts">
-</script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout-footer style="text-align: center;height: 100px;width: 100%;position: absolute;bottom: 0;left: 0">
+    Zoe's Blog
+  </a-layout-footer>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+  props: {
+    msg: String,
+  },
+});
+</script>
