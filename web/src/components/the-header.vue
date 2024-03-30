38389840286f9a0e56e91d4d702aa24bc87d8a90
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-03-29 21:38:15 -0700

    6-2 增加电子书管理页面； 新页面开发：页面、路由、菜单

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -6,9 +6,15 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1111</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="/home">
+        <router-link to="/">Home Page</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook">
+        <router-link to="/admin/ebook">Ebooks</router-link>
+      </a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">About us</router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
