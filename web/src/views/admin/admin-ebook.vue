e0d3af6572b6cfab7efbfb04f0ba56c5bd74871b
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-04-04 22:46:19 -0700

    6-11 为保存接口增加参数校验

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -83,7 +83,7 @@ export default defineComponent({
     const ebooks = ref();
     const pagination = ref({
       current: 1,
-      pageSize: 1001,
+      pageSize: 10,
       total: 0
     });
     const loading = ref(false);
@@ -169,16 +169,18 @@ export default defineComponent({
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/ebook/save", ebook.value).then((response) => {
+        modalLoading.value = false;
         const data = response.data; // data = commonResp
         if (data.success) {
           modalVisible.value = false;
-          modalLoading.value = false;
 
           //重新加载列表
           handleQuery({
             page : pagination.value.current,
             size: pagination.value.pageSize
           });
+        } else {
+          message.error(data.message);
         }
       });
     };
