8d16642a3fca29366d071da44301d19644e49a3b
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-04-05 14:00:58 -0700

    squash! 自己调整：footer的style，让它一直在最底部

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,27 +2,23 @@
   <a-layout id="components-layout-demo-top-side-2">
     <the-header></the-header>
     <router-view/>
-    <TheFooter></TheFooter>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
-import TheFooter from "@/components/the-footer.vue";
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
-  props: {
-    msg: String,
-  },
   components: {
     TheHeader,
     TheFooter,
   },
 });
 </script>
-
 
 <style>
 #components-layout-demo-top-side-2 .logo {
