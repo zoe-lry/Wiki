81a99c55db6cc2181f6ff0f2658566c3c50c473b
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-04-16 21:10:26 -0700

    8-7 集成富文本框插件 wangEditor@4.6.3,在文档表单中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -73,6 +73,9 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
@@ -86,6 +89,8 @@ import {message, Modal} from 'ant-design-vue';
 import { Tool } from '@/util/tool';
 import {useRoute} from "vue-router";
 import {ExclamationCircleOutlined} from "@ant-design/icons-vue";
+import E from "wangeditor";
+
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -161,6 +166,8 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    const editor = new E('#content');
+
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
@@ -256,6 +263,9 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function () {
+        editor.create();
+      }, 100);
     };
 
     /**
@@ -271,7 +281,9 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-
+      setTimeout(function () {
+        editor.create();
+      }, 100);
     };
     /**
      * 删除
