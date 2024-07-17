dcb82210a8797716b7e46a36bee50b148155bebd
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-07-16 21:48:31 -0700

    9-4 增加后端校验 -- 修改的时候，用户名不允许修改

--- web/src/views/admin/admin-user.vue
+++ web/src/views/admin/admin-user.vue
@@ -58,7 +58,7 @@
   >
     <a-form :model="user" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
       <a-form-item label="Login Name">
-        <a-input v-model:value="user.loginName" />
+        <a-input v-model:value="user.loginName" :disabled="!!user.id"/>
       </a-form-item>
       <a-form-item label="User Name">
         <a-input v-model:value="user.name" />
