158e7b8ebf3c728219afea338a12fa74001cbdd3
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-07-23 13:55:33 -0700

    9-9 使用vuex保存用户信息，方便其它组件获取登录信息。遗留问题，刷新后值没有了

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,14 +1,23 @@
 <template>
   <a-layout-footer style="text-align: center;height: 100px;width: 100%;position: absolute;bottom: 0;left: 0">
-    Zoe's
+    Zoe's. Welcome {{user.name}}
   </a-layout-footer>
 </template>
 
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent , computed} from 'vue';
+  import store from "@/store";
 
   export default defineComponent({
-    name: 'the-footer'
-  });
+    name: 'the-footer',
+    setup() {
+      const user = computed(() => store.state.user);
+
+      return {
+        user
+      }
+    }
+  })
+
 </script>
