874c28de22708b8dab7addfe1003b4f9b77e14c2
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-07-23 13:28:51 -0700

    9-9 增加redis测试接口

--- src/main/java/com/zoe/wiki/controller/TestController.java
+++ src/main/java/com/zoe/wiki/controller/TestController.java
@@ -4,15 +4,22 @@ package com.zoe.wiki.controller;
 import com.zoe.wiki.domain.Test;
 import com.zoe.wiki.service.TestService;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //返回一个字符串
 public class TestController {
+
+  private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
   //增加一个自定义的配置项示例
   //可以添加默认值， 如果没有配置会采用默认值
@@ -21,7 +28,8 @@ public class TestController {
 
   @Resource
   private TestService testService;
-
+  @Resource
+  private RedisTemplate redisTemplate;
   /**
    * HTTP请求总共有八种，常用的四种请求：GET, POST, PUT, DELETE
    *
@@ -45,5 +53,19 @@ public class TestController {
   @GetMapping("/test/list")
   public List<Test> list() {
     return testService.list();
+  }
+
+  @RequestMapping("/redis/set/{key}/{value}")
+  public String set(@PathVariable Long key, @PathVariable String value) {
+    redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+    LOG.info("key: {}, value: {}", key, value);
+    return "success";
+  }
+
+  @RequestMapping("/redis/get/{key}")
+  public Object get(@PathVariable Long key) {
+    Object object = redisTemplate.opsForValue().get(key);
+    LOG.info("key: {}, value: {}", key, object);
+    return object;
   }
 }
