38fc8c8d911b8ad1e173b98fb4f89d59fd04243b
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-07-24 17:10:09 -0700

    10-6 首页显示电子书信息：文档数、阅读数、点赞数 doc/vote/** 接口不需要登录拦截

--- src/main/java/com/zoe/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/zoe/wiki/config/SpringMvcConfig.java
@@ -22,6 +22,7 @@ public class SpringMvcConfig implements WebMvcConfigurer {
             "/category/all",
             "/ebook/list",
             "/doc/all/**",
+            "/doc/vote/**",
             "/doc/find-content/**"
         );
   }
