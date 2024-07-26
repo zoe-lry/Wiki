9115e01a10fc7eb1a49020a16d8aeae8ee761d52
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-07-26 16:54:46 -0700

    前端增加统一拦截，判断状态码是401 跳转到首页或登录页

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue';
+import Antd, {message} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
 import axios from "axios";
@@ -29,6 +29,15 @@ axios.interceptors.response.use(function (response) {
   return response;
 }, error => {
   console.log('返回错误：', error);
+  const response = error.response;
+  const status = response.status;
+  if (status === 401) {
+    // 判断状态码是401 跳转到首页或登录页
+    console.log("未登录，跳到首页");
+    store.commit("setUser", {});
+    message.error("未登录或登录超时");
+    router.push('/');
+  }
   return Promise.reject(error);
 });
 
