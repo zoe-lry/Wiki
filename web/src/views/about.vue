f228e12d86d1d3974b7615e89cf30e99bd7e6485
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2024-07-26 14:35:29 -0700

    11-9 关于我们文案修改

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>About us</h1>
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <h1>Hi，I'm Zoe.</h1>
+    </a-layout-content>
+  </a-layout>
 </template>
